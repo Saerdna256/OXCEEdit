20a96a395826a9d14971d9f0b50d43df80e30195
Andreas Bonenkamp <andreas_bonenkamp@web.de>
2026-05-26 13:32:00 +0200

    general savedata display design

--- UIMain.py
+++ UIMain.py
@@ -5,25 +5,54 @@ from ttkbootstrap.dialogs import MessageDialog
 from ttkbootstrap.icons import Icon
 from ttkbootstrap.constants import * # pyright: ignore[reportWildcardImportFromLibrary]
 
+
+
 class MainWindow(ttk.Window):
     def __init__(self) -> None:
         super().__init__(themename="cyborg", title="OXCEEdit")        
         self.geometry("1280x720")
 
         # custom variables
-        self.dataChanged : bool = True    # Any unsaved changes?
+        self.dataChanged : bool = False    # Any unsaved changes?
         self.savefile_path : str = "" # Path to current savefile
+        self.savedataName : str = "Dummy"
 
         # event handlers
         self.protocol("WM_DELETE_WINDOW", self.custom_exit_handler)
 
-        # UI layout
+        # UI Elements definitions
+        ##########################################
         """ For reference:
         Top Level has three element, displayed top to bottom over the full width. Top and bottom are only as high as needed with the center expending as screenspace allows 
         1) Savefile title, path to savefile and Load File button. Neither the titel nor the path can be edited (directly)
-        2) Table of the soldiers in a tabbed view, every tab is a base
+        2) Table of the soldiers in a tabbed view, every tab is a base. Double-clicking a soldier opens the edit soldier dialog.
         3) Funds (can be edited directly) leftbound, Save button in the center, Exit button rightbound 
+        
         """
+        # Frame for top elements        
+        topleveltoprow = ttk.Labelframe(self, labelanchor=NW, text="Savefile", bootstyle=PRIMARY)
+        topleveltoprow.columnconfigure(0, weight=0)
+        topleveltoprow.columnconfigure(1, weight=0)
+        topleveltoprow.columnconfigure(2, weight=1)
+
+        # Top Elements
+        saveTitleInput = ttk.Entry(topleveltoprow, bootstyle=PRIMARY, textvariable=self.savedataName, takefocus=False, state=READONLY, width=64)
+        saveTitleLabel = ttk.Label(topleveltoprow, bootstyle=PRIMARY, text="Savedata Name: ")
+        loadButton = ttk.Button(topleveltoprow, text="Load File...", padding=5)
+        
+        # UI Elements placement
+        ##########################################
+        # Main window
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        # Top Elements into frame
+        saveTitleLabel.grid(column=0, row=0, sticky=W, padx =5, pady = 5)
+        saveTitleInput.grid(column=1, row=0, sticky=W, padx =5, pady = 5)        
+        loadButton.grid(column=2, row=0, sticky=E, padx=5, pady=5)
+
+        # Frames into window        
+        topleveltoprow.grid(column=0, row=0, sticky=NSEW, ipadx=5, ipady=5, padx=5, pady=5)
 
 
     def custom_exit_handler(self) -> None:
@@ -32,11 +61,7 @@ class MainWindow(ttk.Window):
             dialog.show()            
             if dialog.result != "Exit":
                 return
-        self.destroy()
-
-    def custom_exit(self) -> None:
-        self.destroy()
-    
+        self.destroy()        
 
 #####################################################################
 ## DEBUG to quickly test this window alone:
